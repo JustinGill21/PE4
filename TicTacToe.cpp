ec2f3353ea515882d041a784ccca3c7a8a4ce7a2
Meigs2 <ConnorRadeloff1@gmail.com>
2020-02-13 17:57:40 -0700

    Added DisplayBoard function.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,9 +2,35 @@
 
 using namespace std;
 
+const int _boardSize = 3;
+
 int main()
 {
 
 
   return 0;
+}
+
+void DisplayBoard(int arr[_boardSize][_boardSize]) {
+  for (int i = 0; i < _boardSize; i++)
+  {
+    for (int j = 0; j < _boardSize; j++)
+    {
+      string value;
+      if (arr[i][j] == 1)
+      {
+        value = "X";
+      }
+      else if (arr[i][j] == -1 ? "O")
+      {
+        value = "O"
+      }
+      else
+      {
+        value = " "
+      }
+      cout << value << " " << endl; 
+    }
+    cout << "\n";
+  }
 }
