2b158452e2f11ef0a464dd49691bb0bb56ba4a33
Justin Gill <justingill@cu-tcom-2-10.203.8.60.int.colorado.edu>
2020-02-13 18:08:07 -0700

    added DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,9 +2,36 @@
 
 using namespace std;
 
+void DisplayBoard(int myarr[3][3])
+{
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      if(myarr[i][j] == 1){
+        cout << "X" << " ";
+      }
+      else if(myarr[i][j] == 0){
+        cout << " " << " ";
+      }
+      else{
+        cout << "O" << " ";
+      }
+    }
+    cout << endl;
+  }
+}
+
 int main()
 {
+  int myArray[3][3];
+  for(int i = 0; i < 3; i++)
+  {
+    for(int j = 0; j < 3; j++)
+    {
+      myArray[i][j] = 0;
+    }
+  }
 
+  DisplayBoard(myArray);
 
   return 0;
 }
